9db8b5b176e67228b2340ecbc8843d32a189b43a
Michael Clark <micl@umich.edu>
2019-06-14 17:06:15 -0400

    add data, content

---
+++ get_all_results.R
@@ -0,0 +1,203 @@
+## see also https://github.com/tidyverse/dplyr/blob/master/data-raw/starwars.R#L36
+# library(httr)
+# get_all <- function(url) {
+#   out <- NULL
+#   
+#   while (!is.null(url)) {
+#     message("Getting ", url)
+#     req <- GET(url)
+#     stop_for_status(req)
+#     
+#     con <- content(req)
+#     out <- c(out, con$results)
+#     url <- con$`next`
+#   }
+#   
+#   out
+# }
+# 
+# lookup <- function(url, name = "name") {
+#   all <- get_all(url)
+#   
+#   url <- all %>% map_chr("url")
+#   name <- all %>% map_chr(name)
+#   name[name == "unknown"] <- NA
+#   
+#   set_names(name, url)
+# }
+
+
+get_all_results = function(fn) {
+  result_list = list()
+  result_list[[1]] = fn(parse_result = T)
+  
+  # if no additional results
+  if (is.null(getElement(result_list[[1]], "next"))) {
+    init = map(result_list, function(x) x$results)
+    init = map_depth(init, 3, function(x) ifelse(length(x) > 1, paste0(x, collapse = ' '), x))
+    init = map_depth(init, 2, as_tibble)
+    
+    result = bind_rows(map(init, bind_rows))
+  } else {
+    page = as.integer(str_sub(getElement(result_list[[1]], "next"), start = -1))
+    
+    # page should be null if nothing left
+    while(length(page)) {
+      if (page == 2) {
+        current_result = fn(getElement(result_list[[1]], "next"), parse_result = TRUE)
+      } else {
+        last_result = result_list[[page - 1]]
+        current_result = fn(getElement(last_result, "next"), parse_result = TRUE)
+      }
+      
+      result_list[[page]] = current_result
+      
+      page = as.integer(str_sub(getElement(current_result, "next"), start = -1))
+      # get the actual results that are desired
+      init = map(result_list, function(x) x$results)
+      
+      # to deal with multiple entry columns (flatten won't work because they aren't a list)
+      init = map_depth(init, 3, function(x) ifelse(length(x) > 1, paste0(x, collapse = ' '),
+                                                   ifelse(is.null(x), NA, x)))
+      
+      # make a data frame and bind together
+      init = map_depth(init, 2, as_tibble)
+      result = bind_rows(map(init, bind_rows))
+    }
+  }
+  
+  
+  # more misc cleanup
+  nams = names(result)
+  
+  result = result %>% 
+    mutate_all(function(x) ifelse(x == 'n/a' | x == 'unknown' | x == 'none', NA, x)) %>% 
+    mutate(index = as.integer(str_extract_all(url, '[0-9]+'))) %>% 
+    mutate_if(function(x) any(grepl(unlist(x), pattern = '^http')), # make urls indices
+              function(x) sapply(str_extract_all(x, '[0-9]+'), as.integer)) %>% 
+    arrange(index)
+  
+  # specific fixes
+  
+  # people
+  if (any(result$name == 'Luke Skywalker', na.rm = T)) {
+    result = result %>% 
+      mutate(
+        birth_year = str_extract_all(birth_year, '[0-9]+'),
+        mass = as.numeric(mass),
+        height = as.numeric(height)
+      )
+  }
+  
+  # films none
+  
+  # planets
+  if (any(result$name == 'Tatooine', na.rm = T)) {
+    result = result %>% 
+      mutate_at(vars(rotation_period, orbital_period, diameter, surface_water, population),
+                as.numeric) %>% 
+      mutate(gravity = str_extract_all(gravity, '[0-9]+'))
+  }
+  
+  # starships (fix consumables some other time)
+  if (any(result$name == 'Star Destroyer' | result$name == 'Sand Crawler', na.rm = T)) {
+    result = result %>% 
+      mutate_at(vars(cost_in_credits, max_atmosphering_speed, crew, passengers, 
+                     cargo_capacity),
+                as.numeric) %>% 
+      mutate(length = str_remove_all(length, ','),   # fun!
+             length = as.numeric(length))
+    
+    if (any(result$name == 'Star Destroyer', na.rm = T)) {
+      result = result %>% 
+        mutate(starship_class = tolower(starship_class)) %>% 
+        mutate_at(vars(hyperdrive_rating, MGLT), as.numeric)  # not in vehicles
+    }
+    
+    if (any(result$name == 'Sand Crawler', na.rm = T)) {
+      result = result %>% 
+        mutate(vehicle_class = tolower(vehicle_class))
+    }
+  }
+
+  
+  # species
+  if (any(result$name == 'Wookiee', na.rm = T)) {
+    result = result %>% 
+      mutate(
+        average_height = as.numeric(average_height),
+        average_lifespan = ifelse(average_lifespan == 'indefinite', NA, average_lifespan),
+        average_lifespan = as.numeric(average_lifespan)
+      )
+  }
+  
+  result
+}
+
+
+convert_to_film = function(data) {
+  nams = names(data)
+  
+  if ('films' %in% nams)
+    mutate(data, films = sapply(data[['films']], function(idx) film_names[idx]))
+  else data
+}
+
+convert_to_people = function(data) {
+  nams = names(data)
+  
+  if ('characters' %in% nams)
+    data =  mutate(data, characters = sapply(data[['characters']], function(idx) people_names[idx]))
+  if ('pilots' %in% nams)
+    data =  mutate(data, pilots = sapply(data[['pilots']], function(idx) people_names[idx]))
+  if ('people' %in% nams)
+    data =  mutate(data, people = sapply(data[['people']], function(idx) people_names[idx]))
+  
+  data
+}
+
+convert_to_species = function(data) {
+  nams = names(data)
+  
+  if ('residents' %in% nams)
+    data = mutate(data, residents = sapply(data[['residents']], function(idx) species_names[idx]))
+  if ('species' %in% nams)
+    data = mutate(data, species = sapply(data[['species']], function(idx) species_names[idx]))
+  
+  data
+}
+
+convert_to_planets = function(data) {
+  nams = names(data)
+  
+  if ('planets' %in% nams)
+    data = mutate(data, planets = sapply(data[['planets']], function(idx) planets_names[idx]))
+  if ('homeworld' %in% nams)
+    data = mutate(data, homeworld = sapply(data[['homeworld']], function(idx) planets_names[idx]))
+  
+  data
+}
+
+convert_to_vehicles = function(data) {
+  nams = names(data)
+  
+  if ('vehicles' %in% nams)
+    mutate(data, vehicles = sapply(data[['vehicles']], function(idx) vehicles_names[idx]))
+  else data
+}
+
+convert_to_starships = function(data) {
+  nams = names(data)
+  if ('starships' %in% nams)
+    mutate(data, starships = sapply(data[['starships']], function(idx) starships_names[idx]))
+  else data
+}
+
+convert_all = function(data) {
+  data %>% 
+    convert_to_film() %>% 
+    convert_to_species() %>% 
+    convert_to_planets() %>% 
+    convert_to_vehicles() %>% 
+    convert_to_starships()
+}
--- starwars.R
+++ starwars.R
@@ -1,8 +1,11 @@
+library(tidyverse)
 library(rwars)
 
+
+# get preliminary results -------------------------------------------------
+
 films = get_all_films(parse_result = T)
-people1 = get_all_people(parse_result = T)
-people2 = get_all_people(getElement(people1,"next"), parse_result = T)
+people = get_all_people(parse_result = T)
 planets = get_all_planets(parse_result = T)
 species = get_all_species(parse_result = T)
 starships = get_all_starships(parse_result = T)
@@ -16,29 +19,62 @@ all = list(films = films,
            vehicles = vehicles)
 
 
- save.image('data/starwars.RData')
+save.image('data/starwars_raw.RData')
 
 
-map(all, function(x) data.frame(x$results))
-films$results
-tibble(people$results)
+load('data/starwars_raw.RData')
 
 
-people_df = map_df(people$results, function(x) as_tibble(x[1:10]))
+source('get_all_results.R')
 
-get_all_results = function(fn) {
-  res = fn(parse_result = T)
-  if (is.null(getElement(res, "next"))) return(res)
-  page = as.integer(str_sub(getElement(res, "next"), start = -1))
-  
-  while(page > 1) {
-    init =   fn(getElement(res, "next"))
-    res = append(res, init))
-    page = as.integer(str_sub(getElement(res, "next"), start = -1))
-  }
-  
-  res
-}
 
-debugonce(get_all_results)
-test = get_all_results(get_all_planets)
+# get all the results (for real) ------------------------------------------
+
+films_df = get_all_results(get_all_films)
+people_df = get_all_results(get_all_people)
+planets_df = get_all_results(get_all_planets)
+species_df = get_all_results(get_all_species)
+starships_df = get_all_results(get_all_starships)
+vehicles_df = get_all_results(get_all_vehicles)
+
+
+# extract names -----------------------------------------------------------
+
+
+film_names = films_df$title
+people_names = people_df$name
+planets_names = planets_df$name
+species_names = species_df$name
+starships_names = starships_df$name
+vehicles_names = vehicles_df$name
+
+save(film_names,
+     people_names,
+     planets_names,
+     species_names,
+     starships_names,
+     vehicles_names,
+     file = 'data/starwars_names.RData')
+
+
+
+# convert the former urls to labeled values -------------------------------
+
+films_df     = convert_all(films_df)
+people_df    = convert_all(people_df)
+planets_df   = convert_all(planets_df)
+species_df   = convert_all(species_df)
+starships_df = convert_all(starships_df)
+vehicles_df  = convert_all(vehicles_df)
+
+
+save(films_df,
+     people_df,
+     planets_df,
+     species_df,
+     starships_df,
+     vehicles_df,
+     file = 'data/starwars_df.RData')
+
+
+
